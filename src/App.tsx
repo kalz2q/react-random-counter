decb7162522d335aaa94d046cc936fcfcd1d8beb
kalz2q <katsu95i@gmail.com>
2022-08-25 03:20:44 +0900

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+
+import "./App.css";
+
+const getRandomInt = (max: number) =>
+  1 + Math.floor(Math.random() * Math.floor(max));
 
 function App() {
+  const [count, setCount] = useState(0);
+  const ref = useRef(0);
+  console.log(count);
+
+  useEffect(() => {
+    ref.current = getRandomInt(10);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Random countup: {count}</p>
+      <button onClick={() => setCount(prevCount + 1)}>
+        {/* <button onClick={() => setCount(count + ref.current)}> */}
+        random countup
+      </button>
     </div>
   );
 }
